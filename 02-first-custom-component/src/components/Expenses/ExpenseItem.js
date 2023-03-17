6bf71044eaf25a0be31be5b994648ef22e50ceb3
zarnocha <artur.zarnoch@student.uwm.edu.pl>
2023-03-18 00:41:34 +0100

    Section 5: lesson 67. A form with adding a new expense, showing all expenses and filter them by a filtering select element

--- 02-first-custom-component/src/components/Expenses/ExpenseItem.js
+++ 02-first-custom-component/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 export default function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    const str = title.split("").reverse().join("");
+    setTitle(str);
+    console.log(title);
+  };
+
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+        <button onClick={clickHandler}>Change Title</button>
+      </Card>
+    </li>
   );
 }
--- 02-first-custom-component/src/components/Expenses/Expenses.js
+++ 02-first-custom-component/src/components/Expenses/Expenses.js
@@ -1,20 +1,25 @@
-import React from "react";
-import ExpenseItem from "./ExpenseItem";
-import Card from "../UI/Card";
+import React, { useState } from "react";
 import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 export default function Expenses(props) {
-  const expenses = props.expenses;
+  // const expenses = props.expenses;
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const EXPENSES_ARRAY_FILTERED = props.expenses.filter((expense) => {
+    return expense.date.getFullYear() === Number(filteredYear);
+  });
 
   return (
     <Card className="expenses">
-      {expenses.map((_object, i) => (
-        <ExpenseItem
-          title={expenses[i].title}
-          amount={expenses[i].amount}
-          date={expenses[i].date}
-        />
-      ))}
+      <ExpensesFilter selected={filteredYear} onSelectYear={filterHandler} />
+      <ExpensesList expenses={EXPENSES_ARRAY_FILTERED} />
     </Card>
   );
 }

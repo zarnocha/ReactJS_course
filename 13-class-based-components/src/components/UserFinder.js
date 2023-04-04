5e6f09aecd436abfdb91eaf2fb9b93bdbe26b377
zarnocha <artur.zarnoch@student.uwm.edu.pl>
2023-04-04 23:55:45 +0200

    Section 13: lesson 168: learned how to use Context in class-based components and what are the limitations related to it

--- 13-class-based-components/src/components/UserFinder.js
+++ 13-class-based-components/src/components/UserFinder.js
@@ -1,14 +1,11 @@
 import { Component, Fragment } from "react";
 import classes from "./UserFinder.module.css";
 import Users from "./Users";
-
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
+import UsersContext from "../store/users-context";
 
 class UserFinder extends Component {
+  static contextType = UsersContext;
+
   constructor() {
     super();
     this.state = {
@@ -19,13 +16,13 @@ class UserFinder extends Component {
 
   componentDidMount() {
     // Send HTTP request...
-    this.setState({ filteredUsers: DUMMY_USERS });
+    this.setState({ filteredUsers: this.context.users });
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.searchTerm !== this.state.searchTerm) {
       this.setState({
-        filteredUsers: DUMMY_USERS.filter((user) =>
+        filteredUsers: this.context.users.filter((user) =>
           user.name.includes(this.state.searchTerm)
         ),
       });

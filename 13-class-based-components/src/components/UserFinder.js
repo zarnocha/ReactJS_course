464d556dcd648f577df4f824207f7c596a9e6c78
zarnocha <artur.zarnoch@student.uwm.edu.pl>
2023-04-05 00:45:06 +0200

    Finished Section 13 - learned about Error boundaries, how to catch errors and handle them. Also learned about class-based components and their lifecycle methods. Learned the difference between functional and class-based components

--- 13-class-based-components/src/components/UserFinder.js
+++ 13-class-based-components/src/components/UserFinder.js
@@ -2,6 +2,7 @@ import { Component, Fragment } from "react";
 import classes from "./UserFinder.module.css";
 import Users from "./Users";
 import UsersContext from "../store/users-context";
+import ErrorBoundary from "./ErrorBoundary";
 
 class UserFinder extends Component {
   static contextType = UsersContext;
@@ -41,7 +42,9 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={this.state.filteredUsers} />
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
       </Fragment>
     );
   }

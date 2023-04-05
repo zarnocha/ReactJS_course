357f62c4f787f8315c8b246cceaf0f1b10b34231
zarnocha <artur.zarnoch@student.uwm.edu.pl>
2023-04-06 00:11:38 +0200

    Section 14: lesson 181: downloaded and updated files from course

--- 14-work-with-http-requests/src/App.js
+++ 14-work-with-http-requests/src/App.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from 'react';
 
-import MoviesList from "./components/MoviesList";
-import "./App.css";
+import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
+import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -11,12 +12,10 @@ function App() {
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
-
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
-
+      const response = await fetch('https://swapi.dev/api/films/');
       if (!response.ok) {
-        throw new Error("Something went wrong!");
+        throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
@@ -29,16 +28,22 @@ function App() {
           releaseDate: movieData.release_date,
         };
       });
-
       setMovies(transformedMovies);
     } catch (error) {
       setError(error.message);
     }
-
     setIsLoading(false);
   }, []);
 
-  let content = <p>Found no movies!</p>;
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
+  let content = <p>Found no movies.</p>;
 
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
@@ -52,12 +57,11 @@ function App() {
     content = <p>Loading...</p>;
   }
 
-  useEffect(() => {
-    fetchMoviesHandler();
-  }, [fetchMoviesHandler]);
-
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>

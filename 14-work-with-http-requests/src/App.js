6994a65155dadd51c84814d219495623131affb5
zarnocha <artur.zarnoch@student.uwm.edu.pl>
2023-04-06 00:05:35 +0200

    Section 14: lesson 180: learned how to use useEffect() hook to properly send a request and how to use it with async keyword. Also learned how to call useCallback() hook on async arrow function

--- 14-work-with-http-requests/src/App.js
+++ 14-work-with-http-requests/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -8,7 +8,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
 
@@ -36,7 +36,7 @@ function App() {
     }
 
     setIsLoading(false);
-  }
+  }, []);
 
   let content = <p>Found no movies!</p>;
 
@@ -51,6 +51,10 @@ function App() {
   if (isLoading) {
     content = <p>Loading...</p>;
   }
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   return (
     <React.Fragment>

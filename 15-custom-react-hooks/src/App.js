c79e10f930792c54d0e7d943325c70f341a97280
zarnocha <artur.zarnoch@student.uwm.edu.pl>
2023-04-08 03:50:40 +0200

    Section 15: lesson 194: created and adjusted custom hook that handles HTTP GET & POST Requests needed for this project

--- 15-custom-react-hooks/src/App.js
+++ 15-custom-react-hooks/src/App.js
@@ -1,44 +1,30 @@
 import React, { useEffect, useState } from "react";
-
+import useHttp from "./hooks/use-http";
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
-    try {
-      const response = await fetch(
-        "https://reactjs-course-3607a-default-rtdb.europe-west1.firebasedatabase.app/tasks.json"
-      );
-
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
+  useEffect(() => {
+    const transformTasks = (tasksObj) => {
       const loadedTasks = [];
 
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
       }
 
       setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    fetchTasks();
-  }, []);
+    };
+    fetchTasks(
+      {
+        url: "https://reactjs-course-3607a-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
+      },
+      transformTasks
+    );
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));

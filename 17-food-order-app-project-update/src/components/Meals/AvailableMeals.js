c24b72a929d4da8a35e90efe495c0af46b66fa0e
zarnocha <artur.zarnoch@student.uwm.edu.pl>
2023-04-10 02:12:52 +0200

    Section 17: lesson 217: meal data is fetched from database and added an information about loading while fetching + error handling

--- 17-food-order-app-project-update/src/components/Meals/AvailableMeals.js
+++ 17-food-order-app-project-update/src/components/Meals/AvailableMeals.js
@@ -1,36 +1,85 @@
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
-import classes from './AvailableMeals.module.css';
-
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+import { useState, useEffect } from "react";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      setIsLoading(true);
+      const response = await fetch(
+        "https://reactjs-course-3607a-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div
+        style={{
+          margin: "auto",
+          marginTop: "3rem",
+          maxWidth: "60rem",
+          width: "90%",
+        }}
+      >
+        <Card>
+          <section className={classes.MealsLoading}>
+            <p>Loading...</p>
+          </section>
+        </Card>
+      </div>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <div
+        style={{
+          margin: "auto",
+          marginTop: "3rem",
+          maxWidth: "60rem",
+          width: "90%",
+        }}
+      >
+        <Card>
+          <section className={classes.MealsError}>
+            <p>{httpError}</p>
+          </section>
+        </Card>
+      </div>
+    );
+  }
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}

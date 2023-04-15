0eebcf51ad6365842fe83cb5feef68539dfd77a3
zarnocha <artur.zarnoch@student.uwm.edu.pl>
2023-04-15 14:34:43 +0200

    Section 19: lesson 253: created a shopping cart with Redux

--- 19-advanced-redux/src/components/Shop/ProductItem.js
+++ 19-advanced-redux/src/components/Shop/ProductItem.js
@@ -1,19 +1,33 @@
-import Card from '../UI/Card';
-import classes from './ProductItem.module.css';
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../store/cart-slice";
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  const dispatch = useDispatch();
+
+  const { title, price, description, id } = props;
+
+  const addToCartHandler = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
+  };
 
   return (
     <li className={classes.item}>
       <Card>
         <header>
           <h3>{title}</h3>
-          <div className={classes.price}>${price.toFixed(2)}</div>
+          <div className={classes.price}>{price.toFixed(2)} zł</div>
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={addToCartHandler}>Add to Cart</button>
         </div>
       </Card>
     </li>
---
+++ 19-advanced-redux/src/store/ui-slice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialCartState = { cartIsVisible: false };
+
+const uiSlice = createSlice({
+  name: "ui",
+  initialState: initialCartState,
+  reducers: {
+    toggle: (state) => {
+      state.cartIsVisible = !state.cartIsVisible;
+    },
+  },
+});
+
+export const uiActions = uiSlice.actions;
+export default uiSlice;

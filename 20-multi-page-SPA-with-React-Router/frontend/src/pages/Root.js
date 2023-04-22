fa2ee47ba987a606822f7e412b0219cec426614f
zarnocha <artur.zarnoch@student.uwm.edu.pl>
2023-04-23 00:57:54 +0200

    Section 20: lesson 289: loading status provided by React Router

--- 20-multi-page-SPA-with-React-Router/frontend/src/pages/Root.js
+++ 20-multi-page-SPA-with-React-Router/frontend/src/pages/Root.js
@@ -1,13 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 import React from "react";
 
 const RootLayout = () => {
+  const navigation = useNavigation();
+
   return (
     <>
       <MainNavigation />
       <main>
+        {navigation.state === "loading" && <p>Loading ...</p>}
         <Outlet />
       </main>
     </>

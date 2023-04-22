f45d680a38ca787b2339293ae98554ee6718b64f
zarnocha <artur.zarnoch@student.uwm.edu.pl>
2023-04-22 02:33:35 +0200

    Section 20: lesson 286: learning about loader function and useLoaderData hook

--- 20-multi-page-SPA-with-React-Router/frontend/src/App.js
+++ 20-multi-page-SPA-with-React-Router/frontend/src/App.js
@@ -17,7 +17,20 @@ const router = createBrowserRouter([
         path: "events",
         element: <EventsRootLayout />,
         children: [
-          { index: true, element: <EventsPage /> },
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: async () => {
+              const response = await fetch("http://localhost:8080/events");
+
+              if (!response.ok) {
+                // incorrect response...
+              } else {
+                const resData = await response.json();
+                return resData.events;
+              }
+            },
+          },
           { path: "new", element: <NewEventPage /> },
           { path: ":eventId", element: <EventDetailPage /> },
           { path: ":eventId/edit", element: <EditEventPage /> },

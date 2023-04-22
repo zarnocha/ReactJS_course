4e8a1cf4938dcd4fbd221c90f98976a9bede5a4a
zarnocha <artur.zarnoch@student.uwm.edu.pl>
2023-04-23 01:32:46 +0200

    Section 20: lesson 290: learned that you can return a Promise object in loader(). Also modified backend not to have delay on returning data. Removed loading state from Root.js

--- 20-multi-page-SPA-with-React-Router/frontend/src/pages/Root.js
+++ 20-multi-page-SPA-with-React-Router/frontend/src/pages/Root.js
@@ -1,16 +1,17 @@
-import { Outlet, useNavigation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+// import { Outlet, useNavigation } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 import React from "react";
 
 const RootLayout = () => {
-  const navigation = useNavigation();
+  // const navigation = useNavigation();
 
   return (
     <>
       <MainNavigation />
       <main>
-        {navigation.state === "loading" && <p>Loading ...</p>}
+        {/* {navigation.state === "loading" && <p>Loading ...</p>} */}
         <Outlet />
       </main>
     </>

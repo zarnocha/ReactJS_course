ce0cc5bd71bacb66d24cb20455130ff073e77b86
zarnocha <artur.zarnoch@student.uwm.edu.pl>
2023-04-23 13:29:37 +0200

    Section 20: lesson 293: handling Routes errors with different status codes, default messages being swapped, throwing Responses

--- 20-multi-page-SPA-with-React-Router/frontend/src/pages/Events.js
+++ 20-multi-page-SPA-with-React-Router/frontend/src/pages/Events.js
@@ -3,6 +3,11 @@ import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const data = useLoaderData();
+
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
+
   const events = data.events;
 
   return <EventsList events={events} />;
@@ -14,9 +19,11 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // incorrect response...
+    // return { isError: true, message: "Could not fetch events." };
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    });
   } else {
     return response;
-    // return resData.events;
   }
 }

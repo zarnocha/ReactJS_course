eab440706d349e7e8dd88ddd8ce5d5b2b5f7afb8
zarnocha <artur.zarnoch@student.uwm.edu.pl>
2023-04-24 00:57:55 +0200

    Section 20: lesson 302: reusing action funcs, learned useActionData hook, handling backend validation on frontend

--- 20-multi-page-SPA-with-React-Router/frontend/src/components/EventForm.js
+++ 20-multi-page-SPA-with-React-Router/frontend/src/components/EventForm.js
@@ -1,8 +1,16 @@
-import { Form, useNavigate, useNavigation } from "react-router-dom";
+import {
+  Form,
+  useNavigate,
+  useNavigation,
+  useActionData,
+  json,
+  redirect,
+} from "react-router-dom";
 
 import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
+  const data = useActionData();
   const navigate = useNavigate();
   const navigation = useNavigation();
 
@@ -13,7 +21,14 @@ function EventForm({ method, event }) {
   }
 
   return (
-    <Form method="POST" className={classes.form}>
+    <Form method={method} className={classes.form}>
+      {data && data.errors && (
+        <ul>
+          {Object.values(data.errors).map((err) => (
+            <li key={err}>{err}</li>
+          ))}
+        </ul>
+      )}
       <p>
         <label htmlFor="title">Title</label>
         <input
@@ -67,3 +82,40 @@ function EventForm({ method, event }) {
 }
 
 export default EventForm;
+
+export async function action({ request, params }) {
+  const method = request.method;
+  const data = await request.formData();
+
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+
+  let url = "http://localhost:8080/events";
+
+  if (method === "PATCH") {
+    const eventId = params.eventId;
+    url += "/" + eventId;
+  }
+
+  const response = await fetch(url, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (response.status === 422) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: "Could not save an event." }, { status: 500 });
+  }
+
+  return redirect("/events");
+}
--- 20-multi-page-SPA-with-React-Router/frontend/src/pages/NewEvent.js
+++ 20-multi-page-SPA-with-React-Router/frontend/src/pages/NewEvent.js
@@ -1,34 +1,8 @@
 import React from "react";
 import EventForm from "../components/EventForm";
-import { json, redirect } from "react-router-dom";
 
 const NewEventPage = () => {
-  return <EventForm />;
+  return <EventForm method="POST" />;
 };
 
 export default NewEventPage;
-
-export async function action({ request, params }) {
-  const data = await request.formData();
-
-  const eventData = {
-    title: data.get("title"),
-    image: data.get("image"),
-    date: data.get("date"),
-    description: data.get("description"),
-  };
-
-  const response = await fetch("http://localhost:8080/events", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(eventData),
-  });
-
-  if (!response.ok) {
-    throw json({ message: "Could not save an event." }, { status: 500 });
-  }
-
-  return redirect("/events");
-}

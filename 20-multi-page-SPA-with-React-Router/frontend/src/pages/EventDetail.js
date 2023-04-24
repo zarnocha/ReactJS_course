632cd7c69f588a4555754047e31f7dcc1a9f8fe5
zarnocha <artur.zarnoch@student.uwm.edu.pl>
2023-04-24 02:43:01 +0200

    Section 20: finished. setTimeout on GET events. Learned about defer, Await with resolve, Suspense with fallback, two requests in defer with keyword await

--- 20-multi-page-SPA-with-React-Router/frontend/src/pages/EventDetail.js
+++ 20-multi-page-SPA-with-React-Router/frontend/src/pages/EventDetail.js
@@ -1,21 +1,38 @@
-import React from "react";
-import { json, redirect, useRouteLoaderData } from "react-router-dom";
+import React, { Suspense } from "react";
+import {
+  json,
+  redirect,
+  useRouteLoaderData,
+  defer,
+  Await,
+} from "react-router-dom";
 import EventItem from "../components/EventItem";
+import EventsList from "../components/EventsList";
 
 const EventDetailPage = () => {
-  const data = useRouteLoaderData("event-detail");
+  const { event, events } = useRouteLoaderData("event-detail");
 
   return (
     <>
-      <EventItem event={data.event} />
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={event}>
+          {(loadedEvent) => <EventItem event={loadedEvent} />}
+        </Await>
+      </Suspense>
+
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={events}>
+          {(loadedEvents) => <EventsList events={loadedEvents} />}
+        </Await>
+      </Suspense>
     </>
   );
 };
 
 export default EventDetailPage;
 
-export async function loader({ request, params }) {
-  const id = params.eventId;
+async function loadEvent(id) {
+  // const id = params.eventId;
 
   const response = await fetch("http://localhost:8080/events/" + id);
 
@@ -25,8 +42,33 @@ export async function loader({ request, params }) {
       { status: 500 }
     );
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.event;
   }
+}
+
+async function loadEvents() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // return { isError: true, message: "Could not fetch events." };
+    // throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+    //   status: 500,
+    // });
+    return json({ message: "Could not fetch events." }, { status: 500 });
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+
+  return defer({
+    event: await loadEvent(id),
+    events: loadEvents(),
+  });
 }
 
 export async function action({ params, request }) {
--- 20-multi-page-SPA-with-React-Router/frontend/src/pages/Events.js
+++ 20-multi-page-SPA-with-React-Router/frontend/src/pages/Events.js
@@ -1,21 +1,22 @@
-import { useLoaderData, json } from "react-router-dom";
+import { useLoaderData, json, defer, Await } from "react-router-dom";
 import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 function EventsPage() {
-  const data = useLoaderData();
+  const { events } = useLoaderData();
 
-  // if (data.isError) {
-  //   return <p>{data.message}</p>;
-  // }
-
-  const events = data.events;
-
-  return <EventsList events={events} />;
+  return (
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
@@ -25,6 +26,13 @@ export async function loader() {
     // });
     return json({ message: "Could not fetch events." }, { status: 500 });
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+export function loader() {
+  return defer({
+    events: loadEvents(),
+  });
 }

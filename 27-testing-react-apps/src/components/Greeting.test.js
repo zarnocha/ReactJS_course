8817310f61a8f98e6a177ad06eb38b43a707afdf
zarnocha <artur.zarnoch@student.uwm.edu.pl>
2023-06-16 00:39:07 +0200

    Section 27: lesson 440: learned about Test Suite

--- 27-testing-react-apps/src/components/Greeting.test.js
+++ 27-testing-react-apps/src/components/Greeting.test.js
@@ -1,16 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test("renders Hello World as a text", () => {
-  // Arrange - render Greeting component
-  render(<Greeting />);
+describe("Greeting component", () => {
+  test("renders Hello World as a text", () => {
+    // Arrange - render Greeting component
+    render(<Greeting />);
 
-  // Act
-  // ...nothing
+    // Act
+    // ...nothing
 
-  // Assert
-  // 13 & 14 === same
-  //   const helloWorldElement = screen.getByText("Hello World", { exact: false });
-  const helloWorldElement = screen.getByText("Hello World!");
-  expect(helloWorldElement).toBeInTheDocument();
+    // Assert
+    // 13 & 14 === same
+    //   const helloWorldElement = screen.getByText("Hello World", { exact: false });
+    const helloWorldElement = screen.getByText("Hello World!");
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 });

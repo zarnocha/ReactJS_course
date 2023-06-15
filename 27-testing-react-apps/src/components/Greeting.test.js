4919e4cb9d2dffe0be2495c30409b5709d028c8d
zarnocha <artur.zarnoch@student.uwm.edu.pl>
2023-06-16 01:37:17 +0200

    Section 27: lesson: 441: learned how to imitate pressing buttons, simulate user events

--- 27-testing-react-apps/src/components/Greeting.test.js
+++ 27-testing-react-apps/src/components/Greeting.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 describe("Greeting component", () => {
-  test("renders Hello World as a text", () => {
+  test('renders "Hello World" as a text', () => {
     // Arrange - render Greeting component
     render(<Greeting />);
 
@@ -14,5 +15,41 @@ describe("Greeting component", () => {
     //   const helloWorldElement = screen.getByText("Hello World", { exact: false });
     const helloWorldElement = screen.getByText("Hello World!");
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test('renders "good to see you" as a text if the button was NOT clicked', () => {
+    render(<Greeting />);
+
+    const goodToSeeYou = screen.getByText("good to see you", { exact: false });
+    expect(goodToSeeYou).toBeInTheDocument();
+  });
+
+  test('renders "Changed!" if the button WAS clicked', () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.getByText("Changed!");
+
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('does not render "good to see you" if the button was clicked', () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.queryByText("good to see you", {
+      exact: false,
+    });
+    expect(outputElement).toBeNull();
   });
 });
